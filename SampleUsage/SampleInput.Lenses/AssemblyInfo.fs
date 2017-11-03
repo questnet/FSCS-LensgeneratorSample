221b1ef6ab169d0216500aac382c9751acc5d792
Michel Bretschneider <mbretsch@questnet.de>
2017-11-03 10:32:37 +0100

    ignore autogenerated lens files

--- SampleUsage/SampleInput.Lenses/AssemblyInfo.fs
+++ SampleUsage/SampleInput.Lenses/AssemblyInfo.fs
@@ -24,16 +24,6 @@ open System.Runtime.InteropServices
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [<assembly: Guid("f0631fda-fccf-4d21-9c43-8f5d7887f4a5")>]
 
-// Version information for an assembly consists of the following four values:
-// 
-//       Major Version
-//       Minor Version 
-//       Build Number
-//       Revision
-// 
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [<assembly: AssemblyVersion("1.0.*")>]
 [<assembly: AssemblyVersion("1.0.0.0")>]
 [<assembly: AssemblyFileVersion("1.0.0.0")>]
 
