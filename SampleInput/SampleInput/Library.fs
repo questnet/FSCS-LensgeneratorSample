2c04ff1ad61c0552232fdecc2bd79ec9c4d4d20e
Michel Bretschneider <mbretsch@questnet.de>
2017-11-03 07:40:27 +0100

    initial commit: a tiny lib
    just a tiny lib to show how to use the lens generator

---
+++ SampleInput/SampleInput/Library.fs
@@ -0,0 +1,45 @@
+﻿namespace SampleInput
+
+open System.IO
+
+module FrontEnd = 
+    type InputProject = {
+        projectName : string
+        projectDir : string
+        projectFileName : string    
+        referencedAssemblies : string list
+    }
+
+type LensLibrary = 
+    | Aether
+
+type OutputFileCreation =
+    | CreateLensFile of (unit -> TextWriter)
+    | CreateLensFileFor of (string -> TextWriter)
+
+type OutputConfig = {
+    FileConfig : OutputFileCreation
+    Namespace : string
+    LensLibrary : LensLibrary
+    CreateErrorWriter : (unit -> TextWriter)
+    UpdateProjectFile : (TextWriter -> string list -> unit)
+}
+
+type LensGenerationParameters = {
+    input : FrontEnd.InputProject
+    output : OutputConfig
+}
+
+type IFormatLenses =
+    abstract member FormatLensFileHeader: unit -> string
+    abstract member FormatLensHeader: string -> string -> string
+    abstract member FormatLens: string -> string -> string -> string -> string -> string
+    abstract member FormatLensFooter: unit -> string
+    abstract member FormatLensFileFooter: unit -> string
+
+type IWriteLenses =
+    abstract member WriteLensFileHeader: unit -> unit
+    abstract member WriteLensHeader: string -> string -> unit
+    abstract member WriteLens: string -> string -> string -> string -> string -> unit
+    abstract member WriteLensFooter: unit -> unit
+    abstract member WriteLensFileFooter: unit -> unit
